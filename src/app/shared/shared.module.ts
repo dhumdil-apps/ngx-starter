829bdc93ede559d20332b539eee173fb03aab3c0
Martin-Peter Lakatos <martin.peter.the.white@gmail.com>
2017-11-26 22:36:41 +0200

    [+] language component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { LanguageComponent } from './language/language.component';
 import { HamburgerComponent } from './hamburger/hamburger.component';
 import { LinkComponent } from './link/link.component';
 import { StaticNavigationComponent } from './static-navigation/static-navigation.component';
@@ -12,11 +13,13 @@ import { StaticNavigationComponent } from './static-navigation/static-navigation
     RouterModule
   ],
   declarations: [
+    LanguageComponent,
     HamburgerComponent,
     LinkComponent,
     StaticNavigationComponent
   ],
   exports: [
+    LanguageComponent,
     HamburgerComponent,
     LinkComponent,
     StaticNavigationComponent,
