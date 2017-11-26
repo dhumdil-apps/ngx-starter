2e68a4b03d9fba32d6f1aa89ef1fb88c6d02fe79
Martin-Peter Lakatos <martin.peter.the.white@gmail.com>
2017-11-26 17:43:20 +0200

    [*] shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HamburgerComponent } from './hamburger/hamburger.component';
+import { LinkComponent } from './link/link.component';
+import { StaticNavigationComponent } from './static-navigation/static-navigation.component';
 
 @NgModule({
-  imports: [],
-  declarations: [],
-  exports: []
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  declarations: [
+    HamburgerComponent,
+    LinkComponent,
+    StaticNavigationComponent
+  ],
+  exports: [
+    HamburgerComponent,
+    LinkComponent,
+    StaticNavigationComponent,
+  ]
 })
 
-export class SharedModule {}
+export class SharedModule
+{}
